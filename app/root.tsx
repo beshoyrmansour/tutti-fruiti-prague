17b14054eef1e3b5c2caf16d817daf41c7c01282
Beshoy R Mansour <beshoy.r.mansour@gmail.com>
2024-03-08 18:19:49 +0200

    coming soon

--- app/root.tsx
+++ app/root.tsx
@@ -9,21 +9,25 @@ import {
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
 import type { LinksFunction } from "@vercel/remix";
+import styles from './style.css'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }, { rel: "stylesheet", href: styles },] : []),
 ];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" style={{ height: '100%', }}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body style={{
+        padding: 0,
+        margin: 0, height: '100%',
+      }}>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,31 @@
-import type { MetaFunction } from "@vercel/remix";
-
+import type { LinksFunction, MetaFunction } from "@vercel/remix";
+import logo from "~/assets/logo.svg";
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Tutti Fruiti Prague" },
+    { name: "description", content: "Welcome to tutti fruiti prague!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 12,
+      height: '100%',
+      padding: 16,
+      fontFamily: "system-ui, sans-serif", lineHeight: "1.8",
+      background: 'linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)'
+    }}>
+      <img src={logo} alt="logo" style={{ maxWidth: '800px' }} />
+      <h1 style={{
+        color: 'white',
+        fontSize: '5rem',
+        fontWeight: 'lighter',
+      }}>Coming soon ...</h1>
+    </ div>
   );
 }
